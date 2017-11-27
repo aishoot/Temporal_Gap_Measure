71236dbfb5f0b028908b05bd48a71b846a953ad4
Chao Peng <pengchaocg@163.com>
2017-11-27 22:38:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 The main program of the temporal gap detection experiment
 ======================================================"""
 
+# Copyright (c) Nov. 2017
+# Chao Peng, Yufan Du, Peking University, Beijing, China
+# All rights reserved.
+
 import tkinter as tk
 from gapGUI import GapDectionGUI
 
